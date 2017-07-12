7e5431cf203e90e4fa41fa9a5f68b4fa405a5cc1
Sutee S <unlimited.tee@gmail.com>
2017-07-12 22:46:01 +0700

    add redux-thunk middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux'
 import { apiMiddleware } from 'redux-api-middleware'
+import thunk from 'redux-thunk'
 import ENV from '../environment'
 import rootReducer from '../reducers'
 
 export default () => {
-    const middlewares = [apiMiddleware]
+    const middlewares = [thunk, apiMiddleware]
     
     if (ENV === "development")
         middlewares.push(createLogger())
