5e20a14afce62ddb7b0e259f5c5c15d5a67ece6b
Sutee S <unlimited.tee@gmail.com>
2017-07-13 18:18:19 +0700

    add header picture

--- src/components/App/Navbar.js
+++ src/components/App/Navbar.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
+import styles from './navbar.css'
+import headerPic from '../../resources/header.png'
 
 const Navbar = ({
-
+    responsive,
+    onClickMenu,
 }) => (
-    <nav>
-        <a href="/">Home</a>
-    </nav>
+    <div>
+        <div className={styles['desktop-only']}>
+            <img src={headerPic} classname={styles['header-pic']} />
+        </div>
+        <nav className={classNames(styles['navbar'], {[styles['responsive']]: responsive})}>
+            <div className={styles['brand']}><a href="#">DeathStar</a></div>
+            <a href="/">Management</a>
+            <a href="#">Bill</a>
+            <a href="#">Promotion</a>
+            <a href="#">Reservation</a>
+            <a href="javascript:void(0);" className={styles['icon']} onClick={() => onClickMenu()}>&#9776;</a>
+        </nav>
+    </div>
 )
 
 class NavbarContainer extends Component {
@@ -15,9 +29,19 @@ class NavbarContainer extends Component {
 
     }
 
+    state = {
+        responsive: false
+    }
+
+    handleClickMenu = () => {
+        this.setState({responsive: ! this.state.responsive})
+    } 
+
     render() {
         return (
-            <Navbar />
+            <Navbar 
+                responsive={this.state.responsive} 
+                onClickMenu={this.handleClickMenu} />
         )
     }
 }
